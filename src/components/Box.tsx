fd47e3e37f11db66501d283b4f106aad8fed0b28
jifang <Tjjifang@163.com>
2019-08-26 13:51:59 +0800

    提交修改

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { DragSource } from "react-dnd";
-import { createDragPreview } from "react-dnd-text-dragpreview";
 
 const style = {
   border: "1px dashed gray",
@@ -12,50 +11,16 @@ const style = {
   float: "left"
 };
 
-const dragPreviewStyle = {
-  backgroundColor: "rgb(68, 67, 67)",
-  borderColor: "#F96816",
-  color: "white",
-  fontSize: 15,
-  paddingTop: 4,
-  paddingRight: 7,
-  paddingBottom: 6,
-  paddingLeft: 7
-};
-
 class Box extends React.Component {
-  public dragPreview;
-
-  componentDidMount() {
-    // handles first time dragPreview setup
-    this.dragPreview = createDragPreview(
-      formatDragMessage(this.props.numRows),
-      dragPreviewStyle
-    );
-    this.props.connectDragPreview(this.dragPreview);
-  }
-  componentDidUpdate(prevProps) {
-    // handles updates to the dragPreview image as the dynamic numRows value changes
-    this.dragPreview = createDragPreview(
-      formatDragMessage(this.props.numRows),
-      dragPreviewStyle,
-      this.dragPreview
-    );
-  }
-
   // 使用 connectDragSource 包裹住 DOM 节点，使其可以接受各种拖动 API
   // connectDragSource 包裹住的 DOM 节点才可以被拖动
   public render() {
     const { isDragging, connectDragSource } = this.props;
-    const { name, Html } = this.props;
+    const { name } = this.props;
     const opacity = isDragging ? 0.4 : 1;
     return (
       connectDragSource &&
-      connectDragSource(
-        <>
-          <div style={{ ...style, opacity }}>{name}</div>
-        </>
-      )
+      connectDragSource(<div style={{ ...style, opacity }}>{name}</div>)
     );
   }
 }
