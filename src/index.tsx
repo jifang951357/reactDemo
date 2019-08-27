1d1f0f40abc6461f337504319c26e7b1eb287303
jifang <Tjjifang@163.com>
2019-08-27 14:18:33 +0800

    提交显示docs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import LayoutContainer from "./main/LayoutContainer";
+import LayoutPage from "./main/router/LayoutPage";
+
 import { Provider } from "react-redux";
 import store from "./main/redux/store";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -10,7 +12,7 @@ import "antd/dist/antd.css";
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
-      <LayoutContainer />
+      <LayoutPage currentUser />
     </Router>
   </Provider>,
   document.getElementById("root")
